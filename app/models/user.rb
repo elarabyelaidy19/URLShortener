acb40391c687e0f506b7793e95b4a3582185f555
elaraby elaidy <elarabyelaidy@gmail.com>
2021-08-07 23:50:07 +0200

    URLS Readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,15 @@ class User < ApplicationRecord
   has_many :submitted_urls, 
     primary_key: :id, 
     foreign_key: :submitted_user_id, 
-    class: :ShortenedUrl
+    class_name: :ShortenedUrl
 
-  has_many: :visits
+  has_many :visits,
     primary_key: :id, 
-    foreign_key :user_id 
+    foreign_key: :user_id, 
     class_name: :USer 
 
-  has_many: :visited_urls, 
-      through: :visits, 
-      source: :shortened_Url
+  has_many :visited_urls, 
+    through: :visits, 
+    source: :shortened_Url
   
 end 
